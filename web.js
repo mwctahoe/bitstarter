a60a8e20f2d6e1a659067f75bb7799d53411b761
mwctahoe <mwctahoe@gmail.com>
2013-07-10 02:35:27 +0000

    displays index.html instead of downloading

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buff = new Buffer(fs.readFileSync('index.html'));
-  response.send(buff);
+  response.send(""+buff);
 });
 
 var port = process.env.PORT || 5000;
